2533f2f5a267f458ab46313469d8d92434a4c729
Yoel N. Fabelo González <human.x7e6@gmail.com>
2023-07-25 18:39:50 -0400

    Refactored

--- script/proyecto.sh
+++ script/proyecto.sh
@@ -4,39 +4,40 @@ if [ "$1" == "run" ]; then
   printf "\x1b[1;31;43mYOU MUST HAVE dotnet v6.0 CORRECTLY INSTALLED IN ORDER TO DEPLOY THIS PROGRAM!\x1b[0m\n"
   cd .. && make dev
 elif [ "$1" == "report" ]; then
-  if [[ $(uname) == "Linux"]]; then
+  if [[ $(uname) == "Linux" ]]; then
     cd ../informe && latexmk && cd ../script
   else
     cd ../informe && pdflatex main.tex && cd ../script
   fi
 elif [ "$1" == "slides" ]; then
-  if [[ $(uname) == "Linux"]]; then
+  if [[ $(uname) == "Linux" ]]; then
     cd ../presentacion && latexmk && cd ../script
   else
     cd ../presentacion && pdflatex main.tex && cd ../script
   fi
 elif [ "$1" == "show_report" ]; then
-  if [[ $(uname) == "Linux"]]; then
+  if [[ $(uname) == "Linux" ]]; then
     cd ../informe && latexmk && xdg-open main.pdf && cd ../script
   else
-    if [[ $(uname) == *"NT"*]]; then
+    if [[ $(uname) == *"NT"* ]]; then
       cd ../informe && pdflatex main.tex && start main.pdf && cd ../script
     else
       cd ../informe && pdflatex main.tex && open main.pdf && cd ../script
     fi
   fi
 elif [ "$1" == "show_slides" ]; then
-  if [[ $(uname) == Linux]]; then
-    cd ../presentacion && latexmk && cd ../script && xdg-open main.pdf && cd ../script
+  if [[ $(uname) == "Linux" ]]; then
+    cd ../presentacion && latexmk && xdg-open main.pdf && cd ../script
   else
-    if [[ $(uname) == *"NT"*]]; then
+    if [[ $(uname) == *"NT"* ]]; then
       cd ../presentacion && pdflatex main.tex && start main.pdf && cd ../script
     else
       cd ../presentacion && pdflatex main.tex && open main.pdf && cd ../script
     fi
   fi
 elif [ "$1" == "clean" ]; then
-  rm
+  cd ../informe && rm -rf **/*.aux **/*.fdb_latexmk **/*.fls **/*.log **/*.nav **/*.vrb **/*.snm **/*.toc **/*.pdf
+  cd ../presentacion && rm -rf **/*.aux **/*.fdb_latexmk **/*.fls **/*.log **/*.nav **/*.vrb **/*.snm **/*.toc **/*.pdf && cd ../script
 else
   printf "Provide an argument:\nrun\nreport\nslides\nshow_report\nshow_slides\nclean\n"
 fi
